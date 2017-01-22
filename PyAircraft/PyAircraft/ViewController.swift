58ee22c3fbcd7dc8917422c52311fa561253620b
田中伟 <tzw@tzw-MBP.local>
2017-01-22 18:21:03 +0800

    Initialize of GUI

---
+++ PyAircraft/PyAircraft/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PyAircraft
+//
+//  Created by 田中伟 on 22/01/2017.
+//  Copyright © 2017 田中伟. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ gui/PyAircraft/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PyAircraft
+//
+//  Created by 田中伟 on 22/01/2017.
+//  Copyright © 2017 田中伟. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
