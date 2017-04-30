1ea680bc641f2d5a4fef4da4353b402d3cbb0fac
田中伟 <tzw@tzw-MBP.local>
2017-04-30 11:01:44 +0800

    new gui project

---
+++ PyAircraft/PyAircraft/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PyAircraft
+//
+//  Created by 田中伟 on 2017/4/30.
+//  Copyright © 2017年 田中伟. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PyAircraft/PyAircraft/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PyAircraft
+//
+//  Created by 田中伟 on 2017/4/30.
+//  Copyright © 2017年 田中伟. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
